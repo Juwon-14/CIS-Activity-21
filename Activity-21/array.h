7dc8f9bca696399044af2e4518c331fc90f2d3df
Juwon-14 <wndnjs9127@gmail.com>
2024-10-29 14:12:28 -0700

    Add files via upload

---
+++ Activity-21/array.h
@@ -0,0 +1,17 @@
+//
+//  array.h
+//  Activity-21
+//
+//  Created by Juwon on 10/1424.
+//
+#ifndef ARRAY_H
+#define ARRAY_H
+
+// Function to fill the array with values
+void fill(int* numbers, int count);
+
+// Function to print the sum of the array elements
+void printSum(const int* numbers, int count);
+
+#endif
+
