a43369a6d0871f9b9eeb9b2b3568a6dc626adcd0
Juwon-14 <wndnjs9127@gmail.com>
2024-10-29 14:12:45 -0700

    Add files via upload

---
+++ Activity-21/array.cpp
@@ -0,0 +1,24 @@
+//
+//  array.cpp
+//  Activity-21
+//
+//  Created by Juwon on 10/14/24.
+//
+
+#include <iostream>
+#include "array.h"
+using namespace std;
+
+void fill(int* numbers, int count) {
+    for (int i = 0; i < count; ++i) {
+        numbers[i] = i + 1;
+    }
+}
+
+void printSum(const int* numbers, int count) {
+    int sum = 0;
+    for (int i = 0; i < count; ++i) {
+        sum += numbers[i];
+    }
+    cout << "Sum of array elements: " << sum << endl;
+}
